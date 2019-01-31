c050ae4e5ccb131303a64b4d0d45b875a26999ea
madycakes <viclee02@gmail.com>
2019-01-30 22:18:01 -1000

    adds exercises

---
+++ mission.js
@@ -0,0 +1,85 @@
+/*Congratulations Prepsters, you've been selected for your FINAL top secret mission. Should you choose to accept it, do a git fetch and a git pull to find your specific mission. After you complete your mission, commit your work and do a pull request.
+
+As always, should you be caught copy and pasta code, the Secretary will disavow any knowledge of your actions. 
+
+This repo will self-destruct in five seconds. Good luck!*/
+
+/*Mission 1: 
+Team Member: 
+
+Create a function that will sum up only even numbers from 0 - 50.
+*/
+
+
+ /*Mission 2: 
+ Team Member: 
+
+ Create a function that will sum up only odd numbers from 0 - 50.
+ */
+
+
+ /*Mission 3: 
+ Team Member: 
+
+ Create a function that will sum up two numbers.
+ */
+
+
+ /*Mission 4: 
+ Team Member: 
+
+ Create a function that will multiply two numbers.
+ */
+
+
+ /*Mission 5:
+ Team Member: 
+
+ Create a function that will iterate through the given array and returns a new array with only the even indexed items.
+ */
+var myMenu = ['carrot cake', 'brocolli', 'lemon bars', 'spinach', 'oreo cheesecake', 'alfalfa sprouts', 'brownies'];
+
+
+/*Mission 6:
+Team Member: 
+
+Create a function that will sum up numbers from 0 - 100. 
+*/
+
+/*Mission 7:
+Team Member:
+
+Create a function that will reverse a string.
+
+
+/*Mission 8:
+Team Member: 
+
+Create a function that will find the multiples of 3 from 0 - 100 and returns a new array of your results.
+*/
+
+
+/*Mission 9:
+Team Member: 
+
+Create a function that will divide the following elements in the array by 2 and return a new array of your results.
+*/
+var numArr = [10, 20, 30, 40, 50];
+
+
+/*Mission 10:
+Team Member: 
+
+Create a function that will find the multiples of 5 from 0 - 100 and returns a new array of your results.
+*/
+
+
+/*Mission 11:
+Team Member: 
+
+Create a function that will iterate through the given array and pushes even indexed items into an array called evenDrinks and odd indexed items into an array named oddDrinks
+ */
+var myDrinks = ['malibu coke', 'gin tonic', 'old fashion', 'lemoncello tonic', 'long island ice tea', 'caipirinha', 'martini'];
+
+
+
